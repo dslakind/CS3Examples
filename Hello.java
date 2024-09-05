a36b6101c5c6712cb9a9c0f1db78ef1f69e2bb49
David Lakind <dslakind@gmail.com>
2024-09-05 15:26:50 -0400

    Updated Hello.java comments

--- Hello.java
+++ Hello.java
@@ -1,7 +1,18 @@
+/**
+ * This class demonstrates how to use the command line arguments to pass
+ * information to the main method.
+ * 
+ * Below are sample terminal commands to compile and run the file from a terminal. 
+ * The file path may be different on other computers. 
+ * PS C:\Users\dlakind\examples> javac Hello.java
+ * PS C:\Users\dlakind\examples> java Hello Mr. Lakind
+ * Hello Mr. Lakind!
+ */
+
 public class Hello 
 {
     public static void main(String[] args) {
 
-        System.out.println("Hello " + args[0] +" " + args[1]"" + "!");
+        System.out.println("Hello " + args[0] +" " + args[1] + "!");
     }    
 }
