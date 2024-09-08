c8cab36e46473c513ac6840290aa05a4a68e5201
David Lakind <dslakind@gmail.com>
2024-09-07 20:29:23 -0400

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * This class demonstrates how to use the command line arguments to compile and run
+ * a Java program
+ * 
+ * Below are sample terminal commands to compile and run the file from a terminal. 
+ * javac HelloWorld.java
+ * java HelloWorld
+ * 
+ * Output: 
+ * Hello World!
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
